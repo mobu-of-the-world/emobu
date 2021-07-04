f6f2ff5a01cab18cee183b2eb84f3f308a4ec832
Kenichi Kamiya <kachick1@gmail.com>
2021-07-04 12:12:22 +0900

    Store user list with localStorage

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A web application to manage participants and timer for mob-programming!
 
 This project currently is Just an Elm clone of [TypeScript & React](https://github.com/mobu-of-the-world/mobu), but this will overtake it in near future!
 
+Latest version is deployed at [here](https://kachick.github.io/mobu-elm/)
+
 ## How to build `index.html`
 
 ```console
--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,21 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode
+import Json.Encode
 import Random
 import Random.List
 
 
-main : Program () Model Msg
+main : Program Json.Encode.Value Model Msg
 main =
     Browser.element
         { init = init
         , view = view
-        , update = update
+        , update = updateWithStorage
         , subscriptions = \_ -> Sub.none
         }
 
@@ -24,9 +26,16 @@ type alias Model =
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( { inputtedUsername = "", usernames = [] }, Cmd.none )
+init : Json.Encode.Value -> ( Model, Cmd Msg )
+init flags =
+    ( case Json.Decode.decodeValue decoder flags of
+        Ok model ->
+            model
+
+        Err _ ->
+            { inputtedUsername = "", usernames = [] }
+    , Cmd.none
+    )
 
 
 type Msg
@@ -54,6 +63,33 @@ update msg model =
 
         Delete username ->
             ( { model | usernames = List.filter (\element -> not (element == username)) model.usernames }, Cmd.none )
+
+
+port setStorage : Json.Encode.Value -> Cmd msg
+
+
+updateWithStorage : Msg -> Model -> ( Model, Cmd Msg )
+updateWithStorage msg oldModel =
+    let
+        ( newModel, cmds ) =
+            update msg oldModel
+    in
+    ( newModel
+    , Cmd.batch [ setStorage (encode newModel), cmds ]
+    )
+
+
+encode : Model -> Json.Encode.Value
+encode model =
+    Json.Encode.object
+        [ ( "usernames", Json.Encode.list Json.Encode.string model.usernames ), ( "inputtedUsername", Json.Encode.string model.inputtedUsername ) ]
+
+
+decoder : Json.Decode.Decoder Model
+decoder =
+    Json.Decode.map2 Model
+        (Json.Decode.field "inputtedUsername" Json.Decode.string)
+        (Json.Decode.field "usernames" (Json.Decode.list Json.Decode.string))
 
 
 view : Model -> Html Msg
