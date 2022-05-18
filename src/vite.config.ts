98d3b9535d358480e74e65b136030da5fdce86e1
Kenichi Kamiya <kachick1@gmail.com>
2022-05-18 11:29:21 +0900

    Remove neeedless config

--- src/vite.config.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vite';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import elmPlugin from 'vite-plugin-elm';
-
-export default defineConfig({
-  plugins: [elmPlugin()],
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "vite";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import elmPlugin from "vite-plugin-elm";
 
 export default defineConfig({
