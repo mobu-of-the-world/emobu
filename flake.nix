9d14801d5ad063eaff951724fb20993da78f5867
Kenichi Kamiya <kachick1@gmail.com>
2023-08-31 16:27:24 +0900

    Prefer nixos-* for nixpath

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
-    # How to update the revision: `nix flake update --commit-lock-file`
+    # Candidate channels
+    #   - https://github.com/kachick/anylang-template/issues/17
+    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
+    # How to update the revision
+    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
